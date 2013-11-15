bff5aa2455688ed9a42cfacc2021112f32996c4b
Brian <mccor140@knights.ucf.edu>
2013-11-15 16:05:08 -0500

    Uploading the blank FileInput module early for testing purposes.

---
+++ RecipeFinder/RecipeFinder/FileInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+
+namespace RecipeFinder
+{
+    public partial class MainWindow : Window
+    {
+        public void FileInput()
+        {
+            
+        }
+
+    }
+}
